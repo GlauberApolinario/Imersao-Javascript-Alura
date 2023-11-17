2df8265d607384bc5917d7b347f9d2fc4ff04ddd
Glauber Apolinario <glauber.apolinario@yahoo.com.br>
2023-11-17 08:03:46 -0300

    Aula 3 completa e inicio da aula 4

---
+++ index3.js
@@ -0,0 +1,26 @@
+let listaFilmes = [];
+listaFilmes[0] = 'https://m.media-amazon.com/images/I/81aA7hEEykL._AC_UF1000,1000_QL80_.jpg';
+listaFilmes[1] = 'https://m.media-amazon.com/images/I/91eOgodm4nL._AC_UF1000,1000_QL80_.jpg'
+listaFilmes[2] = 'https://i.pinimg.com/originals/ff/be/00/ffbe00fd890d76cf2eb7224e58262be3.jpg'
+
+
+// Desafio
+// alterar o for para while
+// verificar extensão se é jpg
+// criar uma outra lista para adicionar os nomes dos filmes 
+
+listaFilmes.forEach((e) => {
+    document.write('<img src="'+e+'">')
+});
+
+
+
+
+
+
+
+
+
+
+
+
